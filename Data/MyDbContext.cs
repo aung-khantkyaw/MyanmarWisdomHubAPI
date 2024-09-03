3e9546b668b5f0f5dbd4a2e595a83c34419ff395
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-09-04 00:06:04 +0630

    [Added] Quiz Feature

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -12,5 +12,6 @@ namespace MyanmarWisdomHubAPI.Data
         public DbSet<Proverb> Proverb { get; set; } = default!;
         public DbSet<MyanmarWisdomHubAPI.Models.Riddle> Riddle { get; set; } = default!;
         public DbSet<MyanmarWisdomHubAPI.Models.Post> Post { get; set; } = default!;
+        public DbSet<MyanmarWisdomHubAPI.Models.Quiz> Quiz { get; set; } = default!;
     }
 }
---
+++ Models/Quiz.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyanmarWisdomHubAPI.Models
+{
+    [Table("quiz")]
+    public class Quiz
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string question { get; set; }
+
+        [Required]
+        public string answer { get; set; }
+
+        [Required]
+        public string option1 { get; set; }
+
+        [Required]
+        public string option2 { get; set; }
+
+        [Required]
+        public string option3 { get; set; }
+
+        [Required]
+        public string option4 { get; set; }
+    }
+}
